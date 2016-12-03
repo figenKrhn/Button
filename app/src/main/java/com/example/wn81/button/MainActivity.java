d6c9c682f15d1db8d3e468ce1a6f596e5bf63a0e
figenKrhn <figenkrhnhrmn@gmail.com>
2016-12-04 00:13:24 +0300

    Button

---
+++ app/src/main/java/com/example/wn81/button/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.wn81.button;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(getApplicationContext(), "Giriş yaptınız...", Toast.LENGTH_LONG).show();
+
+    }
+}
